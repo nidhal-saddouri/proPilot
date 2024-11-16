f1c2570f182d19ea19beb89a9debc04ccfc7b21d
nidhal saddouri <nidhalsaddouri33@gmail.com>
2024-11-16 01:23:04 +0100

    project setup

---
+++ performance-management-app/src/main/java/com/propilot/performance_management_app/PerformanceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.propilot.performance_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceManagementApplication.class, args);
+	}
+
+}
---
+++ performance-management-app/src/test/java/com/propilot/performance_management_app/PerformanceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propilot.performance_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

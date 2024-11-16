918ea10a6998c48bdd7a6a9b9a3edfdf33f54be6
Zayatiaya <166186004+Zayatiaya@users.noreply.github.com>
2024-11-16 15:34:38 +0100

    Update app.module.ts

--- performance-management-app-frontEnd/src/app/app.module.ts
+++ performance-management-app-frontEnd/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SignupComponent } from './signup/signup.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,

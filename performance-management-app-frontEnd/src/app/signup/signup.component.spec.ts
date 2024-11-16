0f50749ec52a845e8a8b7cbdd0960350f06b51f5
Aya <ayazayatiaya@gmail.com>
2024-11-16 15:21:22 +0100

    Renommage du dossier singup en signup et modification des fichiers nécessaires

--- performance-management-app-frontEnd/src/app/signup/signup.component.spec.ts
+++ performance-management-app-frontEnd/src/app/signup/signup.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SingupComponent } from './singup.component';
+import { SignupComponent } from './signup.component';
 
-describe('SingupComponent', () => {
-  let component: SingupComponent;
-  let fixture: ComponentFixture<SingupComponent>;
+describe('SignupComponent', () => {
+  let component: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SingupComponent]
+      declarations: [SignupComponent]
     });
-    fixture = TestBed.createComponent(SingupComponent);
+    fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
